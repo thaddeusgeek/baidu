b094880d16c5a2799bf5b2c1657fc29d949af849
刘明 <charles.liu@corp.elong.com>
2013-09-30 16:28:43 +0800

    spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'baidu'
+#请根据自己实际情况填写
+BAIDU_MAP_KEY = ''
+$username = ''
+$password =''
+$token =''
+$debug = true
+$portfolio_ids = %w()
+$campaign_ids = %w(  )
+$adgroup_ids = %w(  )
+$keyword_ids = %w(  )
+$startDate = (Time.now - 24*3600).utc.iso8601
+$endDate = Time.now.utc.iso8601

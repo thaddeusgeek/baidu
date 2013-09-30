5649d0559592111ae60e21efa60b720d1b5ffb1b
刘明 <charles.liu@corp.elong.com>
2013-09-30 19:07:52 +0800

    spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,16 @@
 require 'baidu'
 #请根据自己实际情况填写
-BAIDU_MAP_KEY = ''
-$searchWord = ''
-$searchAdgroupName = ''
-$searchCampaignName = ''
-$username = ''
-$password = ''
-$token = ''
-# $debug = true
-$portfolio_ids = %w()
-$campaign_ids = %w(  )
-$adgroup_ids = %w(  )
-$keyword_ids = %w(  )
+BAIDU_MAP_KEY = '' #百度地图TOKEN
+$searchWord = '' #根据Word查已投放关键词
+$searchAdgroupName = '' #根据Adgroup查已投放的单元
+$searchCampaignName = '' #根据Campaign查已投放的计划
+$username = '' #账户
+$password = '' #密码
+$token = '' #token
+$debug = false #需要调试时打开
+$portfolio_ids = %w() #帐号id
+$campaign_ids = %w() #计划id
+$adgroup_ids = %w() #单元id
+$keyword_ids = %w() #关键词id
 $startDate = (Time.now - 24*3600).utc.iso8601
 $endDate = Time.now.utc.iso8601

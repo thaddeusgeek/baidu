b5c51baa91c4c9ed89bc8e389de2c92826ed6429
刘明 <charles.liu@corp.elong.com>
2013-10-07 04:35:53 +0800

    adgroup add,update,delete spec

--- spec/sem_adgroup_spec.rb
+++ spec/sem_adgroup_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Baidu::SEM::AdgroupService do
   subject{Baidu::SEM::AdgroupService.new($auth)}
+  let(:options){options = {}}
   describe '#getAllAdgroupId' do
     it "should return hash with correct format calling getAllAdgroupId " do
       # pending('passed')
@@ -48,17 +49,26 @@ describe Baidu::SEM::AdgroupService do
     end
   end
 
-  describe '#addAdgroup' do
+  describe '#addAdgroup #updateAdgroup #deleteAdgroup' do
     # subject {Baidu::SEM::AdgroupService.new($aut)}
-    it "is not implemented yet" do
-      pending("this is pending before we have testing-purpose account")
+    it "is not ready yet" do
+      # campaign_id = $campaign_ids.first
+      adgroupType_add = {:campaignId=>'1885994',:adgroupName=>'adgroupName',:maxPrice=>2}
+      response = subject.addAdgroup({:adgroupTypes => [adgroupType_add]})
+      expect{ApiResponse.verify(response)}.not_to raise_error
+
+      adgroup_id = response[:adgroup_types][:adgroup_id]
+      adgroupType_update = {:adgroupId => adgroup_id, :adgroupName => 'adgroupName2'}
+      # ap adgroupType
+      response = subject.updateAdgroup({:adgroupTypes=>[adgroupType_update]})
+      expect{ApiResponse.verify(response)}.not_to raise_error
+
+      response = subject.deleteAdgroup({:adgroupIds => [adgroup_id]})
+      expect{ApiResponse.verify(response)}.not_to raise_error
+      # it "is not implemented yet" do
+      #   pending("this is pending before we have testing-purpose account")
+      # end
     end
   end
 
-  describe '#updateAccountInfo' do
-    # subject{Baidu::SEM::AdgroupService.new($auth)}
-    it "is not implemented yet" do
-      pending("this is pending before we have testing-purpose account")
-    end
-  end
 end

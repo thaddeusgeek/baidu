c211ac0afa2dc21a69c5b5502f187fe24fbdbe35
刘明 <charles.liu@corp.elong.com>
2013-10-09 15:29:41 +0800

    api response class

---
+++ lib/baidu/response.rb
@@ -0,0 +1,15 @@
+module Baidu
+  class Response
+    attr_accessor :header,:body,:rquota,:quota,:status,:desc,:oprs,:oprtime
+    def initialize(response,name_response_sym)
+      @header = response.header[:res_header]
+      @body = response.body[name_response_sym]
+      @rquota = @header[:rquota].to_i
+      @quota = @header[:quota].to_i
+      @status = @header[:status].to_i
+      @desc = @header[:desc]
+      @oprs = @header[:oprs]
+      @oprtime = @header[:oprtime]
+    end
+  end
+end

fab9edaf548bb5ee9cc318f394e53f9dabe9280b
刘明 <charles.liu@corp.elong.com>
2013-11-01 15:18:27 +0800

    response namespace bug

--- lib/baidu/response.rb
+++ lib/baidu/response.rb
@@ -2,6 +2,23 @@ module Baidu
   class Response
     attr_accessor :header,:body,:rquota,:quota,:status,:desc,:oprs,:oprtime
     def initialize(response,name_response_sym)
+      name_response_sym =
+      case name_response_sym
+        when 'getAllCampaign'
+          'GetAllCampaign'
+        when 'getCampaignByCampaignId'
+          'GetCampaignByCampaignId'
+        when 'addCampaign'
+          'AddCampaign'
+        when 'updateCampaign'
+          'UpdateCampaign'
+        when 'deleteCampaign'
+          'DeleteCampaign'
+        when 'getCampaignByCampaignId'
+          'GetCampaignByCampaignId'
+        else
+          name_response_sym
+      end
       @header = response.header[:res_header]
       @body = response.body[name_response_sym] if(response.body and response.body.has_key?name_response_sym)
       @rquota = @header[:rquota].to_i
